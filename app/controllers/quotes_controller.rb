7fbd998760054ca9644b56fa732ff48d556397b2
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-17 14:45:19 +0200

    feat(quotes): add `show` controller action

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,9 @@ class QuotesController < ApplicationController
     quotes = Quote.all
     render json: QuoteSerializer.new(quotes).serialized_json
   end
+
+  def show
+    @quote = Quote.find(params['id'])
+    render json: QuoteSerializer.new(@quote).serialized_json
+  end
 end
