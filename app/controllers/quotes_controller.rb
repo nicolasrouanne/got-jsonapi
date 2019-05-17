6b53820def37a3b2cdea987d53b9eae7696abb94
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-17 15:13:06 +0200

    feat(quotes): add include param handling in quote controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,19 @@
 class QuotesController < ApplicationController
+  before_action :check_include_params
+
   def index
     quotes_paginated = Quote.all.page(params[:page]).per(params[:per])
-    render json: QuoteSerializer.new(quotes_paginated, serialize_options(quotes_paginated)).serialized_json
+    options = {include: include}.merge(serialize_options(quotes_paginated))
+    render json: QuoteSerializer.new(quotes_paginated, options).serialized_json
   end
 
   def show
     @quote = Quote.find(params['id'])
-    render json: QuoteSerializer.new(@quote, serialize_options).serialized_json
+    options = {include: include}.merge(serialize_options)
+    render json: QuoteSerializer.new(@quote, options).serialized_json
+  end
+
+  def permitted_relationships
+    %i[author]
   end
 end
