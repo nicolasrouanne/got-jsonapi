08303df9fdbcddb3282e37fc571eff57c6cea5e6
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-17 17:32:35 +0200

    chore(seeds): add seeds to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+authors = Author.create([
+  { id: 1, first_name: 'Fernand', last_name: 'Contandin', alias: 'Fernandel' },
+  { id: 2, first_name: 'Claude', last_name: 'Brasseur', alias: 'Jacky Pic' }
+])
+
+Quote.create([
+  { author_id: 1, title: "I ❤️ Pastis", content: "Le pastis, c'est comme les seins : un, c'est pas assez, et trois, c'est trop." },
+  { author_id: 2, title: "Camping", content: "Pastis par temps bleu, pastis délicieux." }
+])
