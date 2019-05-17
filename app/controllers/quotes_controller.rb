32dc019c7e9bda30fa3097a826403394cde06ac3
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-17 14:52:46 +0200

    feat(quotes): add pagination for quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,11 @@
 class QuotesController < ApplicationController
   def index
-    quotes = Quote.all
-    render json: QuoteSerializer.new(quotes).serialized_json
+    quotes_paginated = Quote.all.page(params[:page]).per(params[:per])
+    render json: QuoteSerializer.new(quotes_paginated, serialize_options(quotes_paginated)).serialized_json
   end
 
   def show
     @quote = Quote.find(params['id'])
-    render json: QuoteSerializer.new(@quote).serialized_json
+    render json: QuoteSerializer.new(@quote, serialize_options).serialized_json
   end
 end
