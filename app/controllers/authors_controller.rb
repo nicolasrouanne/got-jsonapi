29adc1fd8f9039b461a74a150bb033d0923d61f6
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-17 17:23:46 +0200

    feat(sparse fieldsets): add to authors and quote

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,13 +3,13 @@ class AuthorsController < ApplicationController
 
   def index
     authors_paginated = Author.all.page(params[:page]).per(params[:per])
-    options = {include: include}.merge(serialize_options(authors_paginated))
+    options = {include: include, fields: fields}.merge(serialize_options(authors_paginated))
     render json: AuthorSerializer.new(authors_paginated, options).serialized_json
   end
 
   def show
     @author = Author.find(params['id'])
-    options = {include: include}.merge(serialize_options)
+    options = {include: include, fields: fields}.merge(serialize_options)
     render json: AuthorSerializer.new(@author, options).serialized_json
   end
 
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,13 +3,13 @@ class QuotesController < ApplicationController
 
   def index
     quotes_paginated = Quote.all.page(params[:page]).per(params[:per])
-    options = {include: include}.merge(serialize_options(quotes_paginated))
+    options = {include: include, fields: fields}.merge(serialize_options(quotes_paginated))
     render json: QuoteSerializer.new(quotes_paginated, options).serialized_json
   end
 
   def show
     @quote = Quote.find(params['id'])
-    options = {include: include}.merge(serialize_options)
+    options = {include: include, fields: fields}.merge(serialize_options)
     render json: QuoteSerializer.new(@quote, options).serialized_json
   end
 
