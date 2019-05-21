18b39e3e161028a827e3932f8c3c5ca004462539
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-21 18:12:25 +0200

    chore(sparse fieldsets): add sparse fieldsets feature
    - for demo purposes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,13 +3,13 @@ class CharactersController < ApplicationController
 
   def index
     characters_paginated = Character.all.page(params[:page]).per(params[:per])
-    options = {include: include}.merge(serialize_options(characters_paginated))
+    options = {include: include, fields: fields}.merge(serialize_options(characters_paginated))
     render json: CharacterSerializer.new(characters_paginated, options).serialized_json
   end
 
   def show
     @character = Character.find(params['id'])
-    options = {include: include}.merge(serialize_options)
+    options = {include: include, , fields: fields}.merge(serialize_options)
     render json: CharacterSerializer.new(@character, options).serialized_json
   end
 
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,13 +3,13 @@ class HousesController < ApplicationController
 
   def index
     houses_paginated = House.all.page(params[:page]).per(params[:per])
-    options = {include: include}.merge(serialize_options(houses_paginated))
+    options = {include: include, fields: fields}.merge(serialize_options(houses_paginated))
     render json: HouseSerializer.new(houses_paginated, options).serialized_json
   end
 
   def show
     @house = House.find(params['id'])
-    options = {include: include}.merge(serialize_options)
+    options = {include: include, fields: fields}.merge(serialize_options)
     render json: HouseSerializer.new(@house, options).serialized_json
   end
   
