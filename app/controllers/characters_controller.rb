3e4d285a61c0944e81615b2d5f3a661b8002c265
Nicolas Rouanne <nico.rouanne@gmail.com>
2019-05-21 17:21:22 +0200

    chore(api): remove pagination and query params
    - for demo purposes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,19 +1,11 @@
 class CharactersController < ApplicationController
-  before_action :check_include_params
-
   def index
-    characters_paginated = Character.all.page(params[:page]).per(params[:per])
-    options = {include: include, fields: fields}.merge(serialize_options(characters_paginated))
-    render json: CharacterSerializer.new(characters_paginated, options).serialized_json
+    characters = Character.all
+    render json: CharacterSerializer.new(characters).serialized_json
   end
 
   def show
     @character = Character.find(params['id'])
-    options = {include: include, fields: fields}.merge(serialize_options)
-    render json: CharacterSerializer.new(@character, options).serialized_json
-  end
-
-  def permitted_relationships
-    %i[house]
+    render json: CharacterSerializer.new(@character).serialized_json
   end
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,19 +1,11 @@
 class HousesController < ApplicationController
-  before_action :check_include_params
-
   def index
-    houses_paginated = House.all.page(params[:page]).per(params[:per])
-    options = {include: include, fields: fields}.merge(serialize_options(houses_paginated))
-    render json: HouseSerializer.new(houses_paginated, options).serialized_json
+    houses = House.all
+    render json: HouseSerializer.new(houses).serialized_json
   end
 
   def show
     @house = House.find(params['id'])
-    options = {include: include, fields: fields}.merge(serialize_options)
-    render json: HouseSerializer.new(@house, options).serialized_json
-  end
-
-  def permitted_relationships
-    %i[characters]
+    render json: HouseSerializer.new(@house).serialized_json
   end
 end
